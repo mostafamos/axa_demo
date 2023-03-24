82d4cff90f3cc884ac458a5b0a841426d2f7839b
Mostafa <mostafa@MacBook-Air-2.local>
2023-03-24 14:03:09 +0900

    added a simple notnull assertion test on service

--- src/test/java/jp/co/axa/apidemo/ApiDemoApplicationTests.java
+++ src/test/java/jp/co/axa/apidemo/ApiDemoApplicationTests.java
@@ -1,16 +1,27 @@
 package jp.co.axa.apidemo;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.isNotNull;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import jp.co.axa.apidemo.repositories.EmployeeRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApiDemoApplicationTests {
 
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
 	@Test
 	public void contextLoads() {
+		assertNotNull(employeeRepository);
 	}
 
 }
